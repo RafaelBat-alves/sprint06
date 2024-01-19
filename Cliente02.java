6f06034d29858e5f98757d98d973ddf3a3467c7b
RAFAEL <rafaelolivais09@gmail.com>
2024-01-19 16:45:55 -0300

    AS 4 , 6 FEITA

--- Cliente02.java
+++ Cliente02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Cliente {
+public class Cliente02 {
     public static void main(String[] args) {
         Scanner Teclado = new Scanner(System.in);
         boolean sair = false;
